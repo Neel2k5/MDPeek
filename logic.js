560cba72e7ce4e8dea12316d8d9799f1db43e8b1
sd2k5 <s.2k5@gmail.com>
2025-04-13 17:40:10 +0530

    Added local storage upon refresh

--- logic.js
+++ logic.js
@@ -1,57 +1,58 @@
-const  clearButton = document.getElementById("clear-button");
+const clearButton = document.getElementById("clear-button");
 const saveButton = document.getElementById("save-button");
 const inputTextArea = document.getElementById("i-ta");
 const previewDiv = document.getElementById("o-pr");
 
-function resizeInput(){
-    inputTextArea.style.height="auto";
-    inputTextArea.style.height=`${inputTextArea.scrollHeight}px`;
+function resizeInput() {
+    inputTextArea.style.height = "auto";
+    inputTextArea.style.height = `${inputTextArea.scrollHeight}px`;
 }
 
-function parseMD(){
+function parseMD() {
     const result = marked.parse(inputTextArea.value);
-    previewDiv.innerHTML=result;
+    localStorage.setItem("markdownData", inputTextArea.value);
+    previewDiv.innerHTML = result;
     resizeInput();
 }
 
-function downloadMD(){
-    const data = inputTextArea.value;
-    if(data == "")return;
-    const dataBlob = new Blob([data],{type:"text/markdown"});
-    const dataURL =URL.createObjectURL(dataBlob);
-    const installationURL = document.createElement("a");
-    installationURL.href = dataURL;
-    installationURL.download="file.md";
-    document.body.appendChild(installationURL);
-    installationURL.click();
-    document.body.removeChild(installationURL); 
-    URL.revokeObjectURL(dataURL); 
-}
-
-//Initial default text
-parseMD();
-resizeInput();
-
-
-
-
-// events
-inputTextArea.addEventListener(
-    "input",
-    parseMD
-);
-
-clearButton.addEventListener(
-    "click",
-    ()=>{
-        const userConfirmed = confirm("Are you sure you want to clear the markdown?");
-        if(userConfirmed)
-        inputTextArea.value="";
-        previewDiv.innerHTML = "";
+function loadLSData() {
+    const savedData = localStorage.getItem("markdownData");
+    if (savedData) {
+        inputTextArea.value = savedData;
+        const result = marked.parse(savedData);
+        previewDiv.innerHTML = result;
         resizeInput();
     }
-);
-saveButton.addEventListener(
-    "click",
-    downloadMD
-);
+}
+
+function downloadMD() {
+    const data = inputTextArea.value;
+    if (data === "") return;
+    const dataBlob = new Blob([data], { type: "text/markdown" });
+    const dataURL = URL.createObjectURL(dataBlob);
+    const installationURL = document.createElement("a");
+    installationURL.href = dataURL;
+    installationURL.download = "file.md";
+    document.body.appendChild(installationURL);
+    installationURL.click();
+    document.body.removeChild(installationURL);
+    URL.revokeObjectURL(dataURL);
+}
+
+loadLSData();
+parseMD();
+
+
+inputTextArea.addEventListener("input", parseMD);
+
+clearButton.addEventListener("click", () => {
+    const userConfirmed = confirm("Are you sure you want to clear the markdown?");
+    if (userConfirmed) {
+        inputTextArea.value = "";
+        previewDiv.innerHTML = "";
+        localStorage.setItem("markdownData", inputTextArea.value);
+        resizeInput();
+    }
+});
+
+saveButton.addEventListener("click", downloadMD);
